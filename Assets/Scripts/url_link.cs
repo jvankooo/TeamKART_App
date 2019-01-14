f4e81cc6e016a8d41e7c3588a222b3eff1f7e91b
Jee Van Kur <40460588+Jee-Van-Kur@users.noreply.github.com>
2019-01-14 11:42:50 +0530

    assets update

---
+++ Assets/Scripts/url_link.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class url_link : MonoBehaviour {
+
+    public string insta;
+    public string facebook;
+    public string twit;
+
+    public void Instagram()
+    {
+        Application.OpenURL(insta);
+    }
+
+    public void Facebook()
+    {
+        Application.OpenURL(facebook);
+    }
+
+    public void Twitter()
+    {
+        Application.OpenURL(twit);
+    }
+}
